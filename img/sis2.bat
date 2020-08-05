0419a4290f86151f395b4f4f1f1cd3441582d11b
chutkoy <chutkoy@users.noreply.github.com>
2020-08-05 13:29:46 +0300

    del

---
+++ img/sis2.bat
@@ -0,0 +1,16 @@
+@echo off
+d:
+cd\msid\d11303
+echo.
+echo.The 1394 Mac is belw:
+echo.
+echo.
+type SIS1394.MAC
+echo.
+echo.
+echo.The LAN Mac is belw:
+echo.
+echo.
+type SISlan.MAC
+echo.
+echo.
